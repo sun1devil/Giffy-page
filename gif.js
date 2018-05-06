60f1f87b5d1676374a883baead173c7938d2c744
sun1devil <codejmc11@gmail.com>
2018-05-05 18:43:57 -0700

    add submit container and update JQuery logic for rating

--- gif.js
+++ gif.js
@@ -1,48 +1,60 @@
 
 
 
-$(document).ready(function(){
+$(document).ready(function()
+{
     // var header = $("<header></header>").text("Funny Crashes")
     var topics = ["funny snowboard fall", "funny bike crashes", " funny skateboard crashes"]
     var buttons = $("#buttons")
-    for (let i = 0; i < topics.length; i++){
+    for (let i = 0; i < topics.length; i++)
+    {
         let button = $("<li>" + topics[i] + "</li>")
         buttons.append(button)
     }
-    buttons.on("click", function (event){
-        $("#images").empty()
-        console.log(event.target.innerHTML)
-        var subject = event.target.innerHTML
-        giffy(subject)
-    })
+    buttons.on("click", function (event)
+        {
+            $("#images").empty()
+            console.log(event.target.innerHTML)
+            var subject = event.target.innerHTML
+            giffy(subject)
+        })
 
 })
 var GIFFYAPIKEY = "g1d4gakXFH1MwChmMWq1CzN4xucfdwlc"
 var GIFFYURL = "http://api.giphy.com/v1/gifs/search?api_key=" + GIFFYAPIKEY + "&q="
-function giffy(subject){
+function giffy(subject)
+{
     let finalurl = GIFFYURL + subject
     finalurl = encodeURI(finalurl)
     var images = $("#images")
     $.get(finalurl, function(data)
-    {
+        {
         // console.log(data["data"][0])
         for (let i=0; i < 10; i++)
-        {
+            {
             console.log(data["data"][i]["images"]["fixed_height_still"]["url"])
             console.log(data["data"][i]["images"]["fixed_height"]["url"])
             console.log(data["data"][i]["rating"])
             let urlstillimage = data["data"][i]["images"]["fixed_height_still"]["url"]
-            let gifimage = data["data"][i]["images"]["fixed_height"]["url"]
+            let urlgifimage = data["data"][i]["images"]["fixed_height"]["url"]
+            let rating = (data["data"][i]["rating"])
             let li = $("<li></li>")
-            let image = $("<img></img>")
-            let rating = $('<p>"rating"</p>')
-            image.attr("src", urlstillimage)
-            li.append(image)
+            let stillimage = $("<img></img>")
+            let gifimage = $("<img></img>")
+            let ratingText = $('<p></p>')
+            stillimage.attr("src", urlstillimage)
+            gifimage.attr("src",urlgifimage)
+            li.append(stillimage)
+            console.log(gifimage)
             images.append(li)
-            rating.append(rating)
-        }
-    })
+            ratingText.append(rating)
+            }
+        })
+}   
 
-    
-    return true
-}
+// //Onclick of image play gif//
+// images.on("click", function (event){
+//     console.log(true) 
+//     })
+
+//onclick of submit, call Giffy for text submitt, displayimages and create new button//
